76a5d6988aa639bbcc8df437df365a6572f0689d
Ellie <eagyeman1234@gmail.com>
2023-12-27 01:30:47 -0500

    add favicon

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from 'react-router-dom'
-// import {Link} from 'react-router-dom'
+
 //styles
-// import styles from '../NavBar/NavBar.module.css'
+import styles from '../NavBar/NavBar.module.css'
 
 
 
@@ -9,16 +9,20 @@ const NavBar = () => {
 
   return (
   <> 
-  <NavLink to='/'> Home 
-  </NavLink>
-  <NavLink to='/'> About 
-  </NavLink>
-  <NavLink to='/'> Studios 
-  </NavLink>
-  <NavLink to='/'> Merch 
-  </NavLink>
-  <NavLink to='/'> Contact
-  </NavLink>
+    <nav className='nav-group'>
+      <div className='nav-container'>
+        <NavLink to='/'> Home 
+        </NavLink>
+        <NavLink to='/'> About 
+        </NavLink>
+        <NavLink to='/'> Studios 
+        </NavLink>
+        <NavLink to='/'> Merch 
+        </NavLink>
+        <NavLink to='/'> Contact
+        </NavLink>
+      </div>
+    </nav>
   </>
   )
 }
