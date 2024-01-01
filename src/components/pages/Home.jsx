99b6abc7fe41a4eb3b8ce39e94f5cf0d08f80635
Ellie <eagyeman1234@gmail.com>
2024-01-01 17:27:10 -0500

    add css to home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,14 @@
-import '../../App.css'
+// import '../../App.css'
+
+import '../pages/Home.module.css'
 
 const Home = () => {
   return (  
-    <h1>HOME</h1>
+    <>
+      <h1 className='home-title'>GLASS STUDIOS 2.0</h1>
+      {/* <p className='home-slogan'>"Where timeless music is made"</p> */}
+    
+    </>
   )
 }
 
