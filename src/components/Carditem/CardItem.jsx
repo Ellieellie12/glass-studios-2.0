baa59dc02218e85f2dda4994219de6fb31b53898
Ellie <eagyeman1234@gmail.com>
2024-01-07 11:09:49 -0500

    import css for cards items

---
+++ src/components/Carditem/CardItem.jsx
@@ -0,0 +1,14 @@
+import styles from '../Carditem/CardItem.module.css'
+
+const CardItem = (props) => {
+  return ( 
+    <>
+      <li className={styles['cards-items']}>
+        
+      </li>  
+    
+    </>
+  )
+}
+
+export default CardItem
