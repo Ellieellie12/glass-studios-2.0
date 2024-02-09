d6f8e419545eac4def2d245e73e2c7f582427834
Ellie <eagyeman1234@gmail.com>
2024-02-09 11:45:25 -0500

    fix vh for about and home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,8 +9,10 @@ const Home = () => {
 
   return (  
     <>
-      <h1 className={styles['home-title']}>GLASS STUDIOS 2.0</h1>
-      <p className={styles['home-slogan']}>"Where timeless music is made"</p>
+      <div className={styles['home-container']}>
+        <h1 className={styles['home-title']}>GLASS STUDIOS 2.0</h1>
+        <p className={styles['home-slogan']}>"Where timeless music is made"</p>
+      </div>
     </>
   )
 }
