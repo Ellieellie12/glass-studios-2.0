f859a3ad739ca47a2c2392b22450d40ffdaf2084
Ellie <eagyeman1234@gmail.com>
2024-02-14 08:33:23 -0500

    work on animation for text on glass studios 2.0

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -10,8 +10,24 @@ const Home = () => {
   return (  
     <>
       <div className={styles['home-container']}>
-        <h1 className={styles['home-title']}>GLASS STUDIOS 2.0</h1>
-        <p className={styles['home-slogan']}>"Where timeless music is made"</p>
+        {/* <h1 className={styles['home-title']}>GLASS STUDIOS 2.0</h1> */}
+        <svg viewBox="0 0 960 300">
+        <symbol className={styles['title']} id="s-text">
+          <text textAnchor="middle" x="50%" y="80%">GLASS STUDIOS 2.0 </text>
+          <text textAnchor="middle" x="52%" y="80%">GLASS STUDIOS 2.0 </text>
+          
+        </symbol>
+
+        <g className= "g-ants">
+          <use xlinkHref="#s-text" className="text-copy"></use>
+          <use xlinkHref="#s-text" className="text-copy"></use>
+          <use xlinkHref="#s-text" className="text-copy"></use>
+          <use xlinkHref="#s-text" className="text-copy"></use>
+          <use xlinkHref="#s-text" className="text-copy"></use>
+        </g>
+      </svg>
+
+        {/* <p className={styles['home-slogan']}>"Where timeless music is made"</p> */}
       </div>
     </>
   )
