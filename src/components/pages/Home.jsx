455a07ae1654ca94cc16aa9e077a5f12b2bf77cb
Ellie <eagyeman1234@gmail.com>
2024-02-19 11:31:18 -0500

    glass studios title animation added

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,26 +9,25 @@ const Home = () => {
 
   return (  
     <>
-      <div className={styles['home-container']}>
+      {/* <div className={styles['home-container']}> */}
         {/* <h1 className={styles['home-title']}>GLASS STUDIOS 2.0</h1> */}
         <svg viewBox="0 0 960 300">
         <symbol className={styles['title']} id="s-text">
           <text textAnchor="middle" x="50%" y="80%">GLASS STUDIOS 2.0 </text>
           <text textAnchor="middle" x="52%" y="80%">GLASS STUDIOS 2.0 </text>
-          
         </symbol>
 
         <g className= "g-ants">
-          <use xlinkHref="#s-text" className="text-copy"></use>
-          <use xlinkHref="#s-text" className="text-copy"></use>
-          <use xlinkHref="#s-text" className="text-copy"></use>
-          <use xlinkHref="#s-text" className="text-copy"></use>
-          <use xlinkHref="#s-text" className="text-copy"></use>
+          <use xlinkHref="#s-text" className={styles['text-copy']}></use>
+          <use xlinkHref="#s-text" className={styles['text-copy']}></use>
+          <use xlinkHref="#s-text" className={styles['text-copy']}></use>
+          <use xlinkHref="#s-text" className={styles['text-copy']}></use>
+          <use xlinkHref="#s-text" className={styles['text-copy']}></use>
         </g>
       </svg>
 
         {/* <p className={styles['home-slogan']}>"Where timeless music is made"</p> */}
-      </div>
+      {/* </div> */}
     </>
   )
 }
