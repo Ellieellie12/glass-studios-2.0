c7d00d168dafb499621e91f090d570412af6eab4
Ellie <eagyeman1234@gmail.com>
2024-02-19 17:27:32 -0500

    change photo for about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,8 @@
 import '../../App.css'
 
 import photo from '../../../public/images/vinyl.jpg'
+
+import phototwo from '../../../public/images/vinyltwo.jpg'
 //styles
 import styles from '../pages/About.module.css'
 
@@ -10,7 +12,7 @@ const About = () => {
     <> 
       <div className={styles['about-container']}>
   
-          <img className={styles['vinyl-img']} src={photo} alt='pink and yellow vinyl'/>
+          <img className={styles['vinyl-img']} src={phototwo} alt='pink and yellow vinyl'/>
         
         <h1 className={styles['about-title']}>Where Timeless Music Is Made </h1>
         <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for our clientele.</p>
