5984e6aa29b960170bee1f2833aa5ddf4c6a2538
Ellie <eagyeman1234@gmail.com>
2024-02-19 16:46:31 -0500

    resize vinyl img

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,5 +1,6 @@
 import '../../App.css'
 
+import photo from '../../../public/images/vinyl.jpg'
 //styles
 import styles from '../pages/About.module.css'
 
@@ -8,8 +9,11 @@ const About = () => {
   return ( 
     <> 
       <div className={styles['about-container']}>
-        <h1 className={styles['about-title']}>About Us </h1>
-        <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for the clientele...</p>
+  
+          <img className={styles['vinyl-img']} src={photo} alt='pink and yellow vinyl'/>
+        
+        <h1 className={styles['about-title']}>Where Timeless Music Is Made </h1>
+        <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for our clientele.</p>
       </div>
     </>
 
