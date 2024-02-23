de2d9d0d6955c1b78ce1b4b90517170f599571ed
Ellie <eagyeman1234@gmail.com>
2024-02-23 01:17:17 -0500

    import photos and adjust background image for about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,22 +1,27 @@
 import '../../App.css'
 
-import photo from '../../../public/images/vinyl.jpg'
+// import photo from '../../../public/images/vinyl.jpg'
 
 import phototwo from '../../../public/images/vinyltwo.jpg'
+// import photothree from '../../../public/images/recording.jpg'
+
 //styles
 import styles from '../pages/About.module.css'
+
 
 
 const About = () => {
   return ( 
     <> 
+    <div className={styles['main-container']}>
       <div className={styles['about-container']}>
-  
-          <img className={styles['vinyl-img']} src={phototwo} alt='pink and yellow vinyl'/>
-        
+          
+          
+        <img className={styles['recording-img']} src={phototwo} alt="" />
         <h1 className={styles['about-title']}>Where Timeless Music Is Made </h1>
         <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for our clientele.</p>
       </div>
+    </div>
     </>
 
   );
