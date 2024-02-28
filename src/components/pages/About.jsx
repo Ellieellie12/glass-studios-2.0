7c58685e73517b8d261ef2547d41ed5241185f53
Ellie <eagyeman1234@gmail.com>
2024-02-28 11:34:10 -0500

    font change on contact and merch page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,7 @@ import '../../App.css'
 
 // import photo from '../../../public/images/vinyl.jpg'
 
-import phototwo from '../../../public/images/vinyltwo.jpg'
+// import phototwo from '../../../public/images/vinyltwo.jpg'
 // import photothree from '../../../public/images/recording.jpg'
 
 //styles
@@ -17,7 +17,7 @@ const About = () => {
       <div className={styles['about-container']}>
           
           
-        <img className={styles['recording-img']} src={phototwo} alt="" />
+        {/* <img className={styles['recording-img']} src={phototwo} alt="" /> */}
         <h1 className={styles['about-title']}>Where Timeless Music Is Made </h1>
         <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for our clientele.</p>
       </div>
