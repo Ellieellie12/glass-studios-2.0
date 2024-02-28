c12eacc7e6945401cdbb98493eb01c5496fa6356
Ellie <eagyeman1234@gmail.com>
2024-02-28 14:59:37 -0500

    add background image to about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,6 @@
 import '../../App.css'
 
-// import photo from '../../../public/images/vinyl.jpg'
+// import photo from '../../../public/images/sunvinyl.jpg'
 
 // import phototwo from '../../../public/images/vinyltwo.jpg'
 // import photothree from '../../../public/images/recording.jpg'
@@ -13,13 +13,15 @@ import styles from '../pages/About.module.css'
 const About = () => {
   return ( 
     <> 
-    <div className={styles['main-container']}>
-      <div className={styles['about-container']}>
-          
-          
-        {/* <img className={styles['recording-img']} src={phototwo} alt="" /> */}
-        <h1 className={styles['about-title']}>Where Timeless Music Is Made </h1>
-        <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for our clientele.</p>
+    {/* <div className={styles['img-background']}>
+      <img className={styles['sun-img']} src={photo} alt="" />
+    </div> */}
+    <div className={styles['img-container']}>
+      <div className={styles['main-container']}>
+        <div className={styles['about-container']}>
+          <h1 className={styles['about-title']}>Where Timeless Music Is Made </h1>
+          <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for our clientele.</p>
+        </div>
       </div>
     </div>
     </>
