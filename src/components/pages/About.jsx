1d87c06c9bf43cc2265679d5e97eb21a364f8638
Ellie <eagyeman1234@gmail.com>
2024-02-29 19:54:23 -0500

    import gradient background img to about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,6 +1,6 @@
 import '../../App.css'
 
-// import photo from '../../../public/images/sunvinyl.jpg'
+import photo from '/images/aboutpage.jpg'
 
 // import phototwo from '../../../public/images/vinyltwo.jpg'
 // import photothree from '../../../public/images/recording.jpg'
@@ -13,17 +13,11 @@ import styles from '../pages/About.module.css'
 const About = () => {
   return ( 
     <> 
-    {/* <div className={styles['img-background']}>
-      <img className={styles['sun-img']} src={photo} alt="" />
-    </div> */}
-    <div className={styles['img-container']}>
       <div className={styles['main-container']}>
-        <div className={styles['about-container']}>
+        <img className={styles['grad-img']} src={photo} alt='gradient blue and green backdrop' />
           <h1 className={styles['about-title']}>Where Timeless Music Is Made </h1>
           <p className={styles['about-paragraph']}>Glass Studios is all about creating a home away from home for our clientele.</p>
-        </div>
       </div>
-    </div>
     </>
 
   );
